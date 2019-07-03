c0725ed691ebb1c7e6d2935ad2ac64f62edd3a6d
AndyGFX <o.kollar@gmail.com>
2019-07-03 07:33:19 +0200

    WIP preview fire setup

--- Scripts/BulletsFramework/BF_Origin.gd
+++ Scripts/BulletsFramework/BF_Origin.gd
@@ -1,3 +1,4 @@
+tool
 extends Node2D
 
 export var fireAngle:float = 0
@@ -5,9 +6,22 @@ export var offsetSpawnPoint:Vector2 = Vector2()
 
 
 func _ready():	
-	$SpawnPoint.position = self.offsetSpawnPoint;
+	
 	pass
 	
 	
-func _draw():
-	Utils.draw_circle_arc(self,Vector2(),64,-45,45,Color.red)
+func preview_setup():
+	
+	Utils.draw_circle_arc(self,$SpawnPoint.position + self.offsetSpawnPoint,64,-self.fireAngle,self.fireAngle,Color.red)
+	self.draw_line(
+		$SpawnPoint.position + self.offsetSpawnPoint,
+		$SpawnPoint.position + self.offsetSpawnPoint+Vector2(8,0),Color.green,2,true)
+	self.draw_line($SpawnPoint.position + self.offsetSpawnPoint+Vector2(0,-2),$SpawnPoint.position + self.offsetSpawnPoint+Vector2(0,2),Color.green,2,true)
+	
+	self.draw_line($SpawnPoint.position+Vector2(0,-2),$SpawnPoint.position+Vector2(0,2),Color.red,2,true)
+	self.draw_line($SpawnPoint.position+Vector2(-2,0),$SpawnPoint.position+Vector2(2,0),Color.red,2,true)
+	
+func _process(delta):
+	if Engine.is_editor_hint():
+		print("draw")
+		preview_setup()
